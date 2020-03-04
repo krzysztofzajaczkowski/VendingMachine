eb9dcdb1c6023b451639b034a1bf99d8f2d6f4ed
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-03-04 22:11:56 +0100

    Add IVendingMachine interface

---
+++ VendingMachine/IVendingMachine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace VendingMachine
+{
+    public interface IVendingMachine
+    {
+        double Credit { get; set; }
+        ICreditCard CreditCard { get; set; }
+        double MaxPrice { get; }
+        List<Product> Products { get; set; }
+        bool UsingCreditCard { get; set; }
+
+        bool BuyProduct(int code);
+        void CheckFunds();
+        bool InsertMoney(double amount);
+        bool PayWithCard(ICreditCard creditCard);
+        void SeeProducts();
+        double Withdraw();
+    }
+}
