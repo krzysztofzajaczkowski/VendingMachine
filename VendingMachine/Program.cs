34fa6608e9dae0847aa11659c9d42e8188351efa
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-03-04 22:14:10 +0100

    Register services and config

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -1,15 +1,36 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace VendingMachine
 {
     class Program
     {
+        public static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
 
+            return builder.Build();
+        }
         private static IServiceCollection ConfigureServices()
         {
             IServiceCollection services = new ServiceCollection();
+
+            var config = LoadConfiguration();
+
+            Console.WriteLine("Start");
+            var snack = config.GetSection("Products").GetSection("Snacks:0").Get<Snack>();
+            Console.WriteLine(snack.Name);
+            Console.WriteLine("Stop");
+            
+
+            services.AddSingleton(config);
+
+            services.AddTransient<IVendingMachine, VendingMachine>();
 
             services.AddTransient<ConsoleApplication>();
 
